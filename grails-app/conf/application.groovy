f6b23a18ab2c6a2963be406bcac69bf4ca17b3fe
HI7 <heb726@g.harvard.edu>
2017-05-02 18:59:37 -0400

    Added security

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -10,7 +10,6 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/user/create',    access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
 	[pattern: '/shutdown',       access: ['permitAll']],
 	[pattern: '/assets/**',      access: ['permitAll']],
 	[pattern: '/**/js/**',       access: ['permitAll']],
